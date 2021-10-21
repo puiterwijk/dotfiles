61c4bfadabf59b8ad6debc05be94251445192ab6
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-10-21 16:05:54 +0200

    Add space after PS1
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- bashrc.d/01-git.sh
+++ bashrc.d/01-git.sh
@@ -7,7 +7,7 @@ export GIT_PS1_SHOWUPSTREAM="auto verbose"
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
 #export PS1='[$? \u@\h \W$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")]\$ '
-export PS1='\[\e]2;\u@\h:\w\a\]\[\]\D{%Y-%m-%d %H:%M:%S}\[\] \[\][\[\]\u@\h\[\]]\[\] \[\][\w]\[\]\n \[`rt=$?; if [ $rt -ne 0 ]; then echo \]🤮\[\]$rt\[; fi`\] ▸'
+export PS1='\[\e]2;\u@\h:\w\a\]\[\]\D{%Y-%m-%d %H:%M:%S}\[\] \[\][\[\]\u@\h\[\]]\[\] \[\][\w]\[\]\n \[`rt=$?; if [ $rt -ne 0 ]; then echo \]🤮\[\]$rt\[; fi`\] ▸ '
 
 export PATH="$PATH:/usr/share/doc/git-1.8.2.1/contrib/remote-helpers/"
 
